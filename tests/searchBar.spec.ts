89580b84bc36a7bf6ee1ce03971e4ba955a6ce51
RanushSanjula <ranush1999san@gmail.com>
2026-09-16 00:19:57 +0530

    Add script for search bar

--- tests/searchBar.spec.ts
+++ tests/searchBar.spec.ts
@@ -3,13 +3,14 @@ import { test, expect } from '@playwright/test';
 test('Login using valid information', async ({ page }) => {
 
 
-  await page.goto('https://demoqa.com/login');
-  await page.locator('id=userName').fill('sf');
-  await page.locator('id=password').fill('Test@12345');
-  await page.locator('id=login').click();
-  await expect(page).toHaveURL('https://demoqa.com/profile');
-  await page.locator('id=gotoStore').click();
-
+    await page.goto('https://demoqa.com/login');
+    await page.locator('id=userName').fill('sf');
+    await page.locator('id=password').fill('Test@12345');
+    await page.locator('id=login').click();
+    await expect(page).toHaveURL('https://demoqa.com/profile');
+    await page.locator('id=gotoStore').click();
+    await page.locator('id=searchBox').fill('Git Pocket Guide');
+    await page.locator('span', { hasText: 'Git Pocket Guide' }).isVisible();
 
 
 })
